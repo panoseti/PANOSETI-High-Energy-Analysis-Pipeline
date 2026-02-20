bc25c7353ef722ccef1e753b427ff1328ef5aa3d
Jamie Holder <jholder@udel.edu>
2026-02-20 11:50:27 -0500

    Update makearrayevents.h
    removed duplicate function definition.

--- eventbuilding/makearrayevents.h
+++ eventbuilding/makearrayevents.h
@@ -79,22 +79,3 @@ TTree * load_camdata(const char *infile)
   camdata->SetBranchAddress("cam_2D_hist",&cam_2D_hist);
   return camdata;
 }
-
-TTree * load_camdata(const char *infile)
-{
-  cout << "reading from " << infile << endl;
-  TFile *root_infile = TFile::Open(infile);
-  TTree *camdata=(TTree*)root_infile->Get("camdata"); //variable declarations are in the .h file
-
-  camdata->SetBranchAddress("cam_npix", &cam_npix);
-  camdata->SetBranchAddress("cam_scope_id", &cam_scope_id);
-  camdata->SetBranchAddress("cam_pcap_time",&cam_pcap_time);
-  camdata->SetBranchAddress("cam_pcap_time_since_start",&cam_pcap_time_since_start);
-  camdata->SetBranchAddress("cam_acq_mode",&cam_acq_mode);
-  camdata->SetBranchAddress("cam_TAI",&cam_TAI);
-  camdata->SetBranchAddress("cam_nanosec",&cam_nanosec);
-  camdata->SetBranchAddress("cam_WR_time",&cam_WR_time);
-  camdata->SetBranchAddress("cam_pix_data",cam_pix_data);
-  camdata->SetBranchAddress("cam_2D_hist",&cam_2D_hist);
-  return camdata;
-}
