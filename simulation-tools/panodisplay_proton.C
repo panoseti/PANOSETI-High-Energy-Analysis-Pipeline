030da713784e3cd82e25afb8acf86ff77778d606
nkorzoun <nkorzoun@udel.edu>
2026-04-21 11:29:19 +0200

    add missing pe conversion and lower thresh to 6.5

--- simulation-tools/panodisplay_proton.C
+++ simulation-tools/panodisplay_proton.C
@@ -1496,7 +1496,7 @@ TH2D* telEvent(int telNumber, int eventNumber){
 
     // trigger threshold
     /*
-    if(image->GetMaximum()<9.0){
+    if(image->GetMaximum()<6.5*petoadu){
         image->Reset();
     }else{
         image = clean(image);
@@ -1507,22 +1507,22 @@ TH2D* telEvent(int telNumber, int eventNumber){
     int npixtrigq1=0, npixtrigq2=0, npixtrigq3=0, npixtrigq4=0;
     for(int i=1; i<=16; i++){
         for(int j=1; j<=16; j++){
-            if(image->GetBinContent(i,j)>9.0) npixtrigq1++;
+            if(image->GetBinContent(i,j)>6.5*petoadu) npixtrigq1++;
         }
     }
     for(int i=17; i<=32; i++){
         for(int j=1; j<=16; j++){
-            if(image->GetBinContent(i,j)>9.0) npixtrigq2++;
+            if(image->GetBinContent(i,j)>6.5*petoadu) npixtrigq2++;
         }
     }
     for(int i=1; i<=16; i++){
         for(int j=17; j<=32; j++){
-            if(image->GetBinContent(i,j)>9.0) npixtrigq3++;
+            if(image->GetBinContent(i,j)>6.5*petoadu) npixtrigq3++;
         }
     }
     for(int i=17; i<=32; i++){
         for(int j=17; j<=32; j++){
-            if(image->GetBinContent(i,j)>9.0) npixtrigq4++;
+            if(image->GetBinContent(i,j)>6.5*petoadu) npixtrigq4++;
         }
     }
 
@@ -1838,22 +1838,22 @@ void showClean(int telNumber, int eventNumber){
     int npixtrigq1=0, npixtrigq2=0, npixtrigq3=0, npixtrigq4=0;
     for(int i=1; i<=16; i++){
         for(int j=1; j<=16; j++){
-            if(image->GetBinContent(i,j)>9.0) npixtrigq1++;
+            if(image->GetBinContent(i,j)>6.5*petoadu) npixtrigq1++;
         }
     }
     for(int i=17; i<=32; i++){
         for(int j=1; j<=16; j++){
-            if(image->GetBinContent(i,j)>9.0) npixtrigq2++;
+            if(image->GetBinContent(i,j)>6.5*petoadu) npixtrigq2++;
         }
     }
     for(int i=1; i<=16; i++){
         for(int j=17; j<=32; j++){
-            if(image->GetBinContent(i,j)>9.0) npixtrigq3++;
+            if(image->GetBinContent(i,j)>6.5*petoadu) npixtrigq3++;
         }
     }
     for(int i=17; i<=32; i++){
         for(int j=17; j<=32; j++){
-            if(image->GetBinContent(i,j)>9.0) npixtrigq4++;
+            if(image->GetBinContent(i,j)>6.5*petoadu) npixtrigq4++;
         }
     }
 
